a918b6255fa92c25bdf0d790e8370537b2e37c09
Valerie <akpotevalerie@gmail.com>
2020-11-20 00:53:01 +0100

    Declare messages to display

--- script.js
+++ script.js
@@ -9,6 +9,15 @@
        return secretNum;
    }
 
+   // declare messages to display
+   let getMessage = (guess, secret, i) => {
+       let win = "Awesome! Your number, " + guess + ", was correct.";
+       let close = "So close, but you misse it. The secret number is " + secret + ".";
+       let lose = "Bummer ... You guessed " + guess + " and the secret number was " + secret + ".";
+       let message = [win, close, lose];
+       return message[i];
+   }
+
    
 
 
