f8af366b209e8ab5a8119660a634ee1ed76f6f56
Valerie <akpotevalerie@gmail.com>
2020-11-21 10:10:30 +0100

    Add styling

--- script.js
+++ script.js
@@ -13,7 +13,7 @@
    let getMessage = (guess, secret, i) => {
        let win = "Awesome! Your number, " + guess + ", is correct.";
        let close = "So close, but you missed it. The secret number is " + secret + ".";
-       let lose = "Bummer ... You guessed " + guess + " and the secret number is" + secret + ".";
+       let lose = "Bummer ... You guessed " + guess + " and the secret number is " + secret + ".";
        let message = [win, close, lose];
        return message[i];
    }
@@ -33,11 +33,12 @@
     } else {
         alert(getMessage(userNum, secretNum, 2));
     };
+    
    }
 
-     // compare numbers when form gets submitted
-     let form = document.getElementById("userInput");
-     form.addEventListener("submit", compareNumbers);
+    // compare numbers when form gets submitted
+    let form = document.getElementById("userInput");
+    form.addEventListener("submit", compareNumbers);
 
 }());
 
